99fe20ba1afcae16c349efcb83619aab24ce2c74
Umamad Mac <marandmohammad@gmail.com>
2023-03-02 19:18:54 +0330

    improve todo tests and add edit todo tests

--- server/src/tests/todo.test.ts
+++ server/src/tests/todo.test.ts
@@ -1,4 +1,11 @@
-import { describe, test, beforeAll, afterAll, jest } from "@jest/globals";
+import {
+  describe,
+  test,
+  beforeAll,
+  afterAll,
+  jest,
+  expect,
+} from "@jest/globals";
 import supertest from "supertest";
 
 import app from "../app";
@@ -6,6 +13,7 @@ import app from "../app";
 jest.setTimeout(20000);
 
 describe("Test todo Apis", function () {
+  let editableTodoId = 0;
   let commonHeaders = {
     Authorization: "",
   };
@@ -39,24 +47,112 @@ describe("Test todo Apis", function () {
       });
   });
 
-  test("Get all todo", async () => {
-    const result = await supertest(app)
-      .get("/todo")
-      .set(commonHeaders)
-      .expect("Content-Type", /json/)
-      .expect(200);
+  describe("Get all todo", () => {
+    test("Should be success", async () => {
+      const result = await supertest(app)
+        .get("/todo")
+        .set(commonHeaders)
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+      expect(typeof result.body).toEqual("object");
+      editableTodoId = result.body[0].id;
+    });
+
+    test("Should be forbidden", async () => {
+      const result = await supertest(app).get("/todo").expect(403);
+    });
   });
 
-  test("Post new todo", async () => {
-    const result = await supertest(app)
-      .post("/todo")
-      .send({
-        title: "jest test todo",
-        description: "jest test description",
-        priority: "high",
-      })
-      .set(commonHeaders)
-      .expect("Content-Type", /json/)
-      .expect(201);
+  describe("Post new todo", () => {
+    test("Should be success", async () => {
+      const result = await supertest(app)
+        .post("/todo")
+        .send({
+          title: "jest test todo",
+          description: "jest test description",
+          priority: "high",
+        })
+        .set(commonHeaders)
+        .expect("Content-Type", /json/)
+        .expect(201);
+    });
+
+    test("Should fail with 400", async () => {
+      const result = await supertest(app)
+        .post("/todo")
+        .send({
+          // title: "jest test todo",
+          description: "jest test description",
+          // priority: "high",
+        })
+        .set(commonHeaders)
+        .expect("Content-Type", /json/)
+        .expect(400);
+    });
+
+    test("Should be forbidden", async () => {
+      const result = await supertest(app)
+        .post("/todo")
+        .send({
+          title: "jest test todo",
+          description: "jest test description",
+          priority: "high",
+        })
+        .expect(403);
+    });
+  });
+
+  describe("Edit todo", () => {
+    test("Should be success", async () => {
+      const result = await supertest(app)
+        .patch(`/todo/${editableTodoId}`)
+        .send({
+          title: "jest test edit todo",
+          description: "jest test edit description",
+          priority: "low",
+        })
+        .set(commonHeaders)
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+
+    test("Should fail not found", async () => {
+      const result = await supertest(app)
+        .patch(`/todo`)
+        .send({
+          title: "jest test edit todo",
+          description: "jest test edit description",
+          priority: "low",
+        })
+        .set(commonHeaders)
+        .expect(404);
+    });
+
+    test("Should fail with 400", async () => {
+      const result = await supertest(app)
+        .patch(`/todo/asda`)
+        .send({
+          title: "jest test edit todo",
+          description: "jest test edit description",
+          priority: "low",
+        })
+        .set(commonHeaders)
+        .expect("Content-Type", /json/)
+        .expect(400);
+
+      expect(result.body.message).toEqual("Invalid id");
+    });
+
+    test("Should be forbidden", async () => {
+      const result = await supertest(app)
+        .patch(`/todo/${editableTodoId}`)
+        .send({
+          title: "jest test todo",
+          description: "jest test description",
+          priority: "high",
+        })
+        .expect(403);
+    });
   });
 });
