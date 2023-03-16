f4a05919425207d415a2f57a9deaff6a12ddc26c
Umamad Mac <marandmohammad@gmail.com>
2023-03-16 16:44:47 +0330

    add interceptors to axios instance

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,6 +1,9 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { toast } from "react-toastify";
 
 import { store } from "../redux/store";
+
+const NETWORK_ERROR = "ERR_NETWORK";
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
@@ -14,7 +17,6 @@ const api = axios.create({
 
 api.interceptors.request.use(
   async (config) => {
-
     // Add accessToken to header
     const state = store.getState();
     const accessToken = state.user.currentUser?.accessToken;
@@ -24,6 +26,22 @@ api.interceptors.request.use(
   },
   (error) => {
     Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error: AxiosError) {
+    // console.log(error);
+    if (error.code === "ERR_NETWORK") {
+      toast.error("Please check your connection", {
+        toastId: NETWORK_ERROR,
+      });
+    }
+
+    return Promise.reject(error);
   }
 );
 
