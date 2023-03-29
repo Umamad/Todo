8ca23148c99d72fdba69f6cc0ad1d7fd70898fd9
Umamad Mac <marandmohammad@gmail.com>
2023-03-29 20:10:56 +0330

    align content to start of page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
     <PageContainer
       sx={{
         p: { xs: 1, sm: 2, md: 4 },
+        alignContent: "flex-start",
       }}
     >
       <Grid container justifyContent="center">
